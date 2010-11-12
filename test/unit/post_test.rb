5ac0f47d70acd6aadb72530e778e0f8137f5bd99
porta <porta@porta-desktop.(none)>
2010-11-12 20:02:06 -0300

    added unit test

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "it fails to create a new Post without a state" do
+    post = Post.create(:title => 'this is title', :body => 'this is body')
+    assert(post.state == 'created', "post without a state")
+    puts "created the following post: #{post.inspect}"
   end
 end
+
