b570c600e7b0c0f9148da601d49145f5eb29681a
porta <porta@porta-desktop.(none)>
2010-11-12 19:26:36 -0300

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestNullWithoutDefault::Application.config.secret_token = '997ec67dfdea2e3e372f19431ea9989b05bb94bab71f7059a7ced051acffbabc5a0780e65ca6742f267f24563c52d3a087780876594a58df3842db2c043bd586'
